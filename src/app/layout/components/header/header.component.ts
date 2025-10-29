17033a32bd19451f4254dbd2ac7a84b58e46fedb
eka <eka.nursa@salt.co.id>
2025-10-29 15:44:31 +0700

    initiate header

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,19 +1,50 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
   @Output() toggleSidebar = new EventEmitter<void>();
+  @Output() search = new EventEmitter<string>();
+  @Output() notificationsClick = new EventEmitter<void>();
+  @Output() languageClick = new EventEmitter<void>();
+  @Output() moreOptionsClick = new EventEmitter<void>();
+
+  // User data matching the Figma design
+  userName = 'Moni Roy';
+  userRole = 'Admin';
+  userAvatar = 'https://picsum.photos/seed/moni-roy-avatar/100/100.jpg';
+  notificationCount = 6;
+  selectedLanguage = 'English';
+  searchQuery = '';
 
   constructor() { }
 
   onToggleSidebar(): void {
     this.toggleSidebar.emit();
+  }
+
+  onSearchChange(event: any): void {
+    this.search.emit(this.searchQuery);
+  }
+
+  onNotificationsClick(): void {
+    this.notificationsClick.emit();
+    // Optional: Clear notification count when clicked
+    // this.notificationCount = 0;
+  }
+
+  onLanguageClick(): void {
+    this.languageClick.emit();
+  }
+
+  onMoreOptionsClick(): void {
+    this.moreOptionsClick.emit();
   }
 }
