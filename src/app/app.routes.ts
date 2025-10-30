4fc0dc00fcb65f32f47100110f9766b51def776c
eka <eka.nursa@salt.co.id>
2025-10-30 11:34:37 +0700

    sidebar styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,6 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProjectsComponent } from './pages/projects/projects.component';
-import { SkillsComponent } from './pages/skills/skills.component';
-import { ExperienceComponent } from './pages/experience/experience.component';
-import { ContactComponent } from './pages/contact/contact.component';
 
 export const routes: Routes = [
   {
@@ -19,22 +15,6 @@ export const routes: Routes = [
       {
         path: 'dashboard',
         component: DashboardComponent
-      },
-      {
-        path: 'projects',
-        component: ProjectsComponent
-      },
-      {
-        path: 'skills',
-        component: SkillsComponent
-      },
-      {
-        path: 'experience',
-        component: ExperienceComponent
-      },
-      {
-        path: 'contact',
-        component: ContactComponent
       }
     ]
   }
--- src/app/pages/projects/projects.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-projects',
-  standalone: true,
-  templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
-})
-export class ProjectsComponent {}
