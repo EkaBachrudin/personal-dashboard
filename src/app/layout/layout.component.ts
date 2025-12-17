aa9f2504f9a5504bd76ac0f0ccaf07289f709535
eka <eka.nursa@salt.co.id>
2025-12-17 18:11:09 +0700

    sidebar mobile

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
@@ -10,8 +10,15 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
   isSidebarOpen = true;
+
+  ngOnInit(): void {
+    // Check if on mobile device and close sidebar by default
+    if (typeof window !== 'undefined' && window.innerWidth < 768) {
+      this.isSidebarOpen = false;
+    }
+  }
 
   toggleSidebar(): void {
     this.isSidebarOpen = !this.isSidebarOpen;
